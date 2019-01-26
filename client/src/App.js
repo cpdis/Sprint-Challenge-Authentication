629f955cf4c0e2edfe6f312c88451130c950296d
Colin Dismuke <colin.dismuke@gmail.com>
2019-01-26 12:53:14 -0600

    Created skeleton App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { NavLink, Route } from "react-router-dom";
+
+import Users from "./components/Users";
+import Home from "./components/Home";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+
+import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
 
 class App extends Component {
+  signOut = () => {
+    localStorage.removeItem("jwt");
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Container />;
   }
 }
 
